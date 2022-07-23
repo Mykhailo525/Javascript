4204a88b9565b83eae08677b0c4ccd5c0ebc7a4c
Mykhailo525 <mtrindak@gmail.com>
2022-07-23 12:09:41 +0300

    Initial commit

---
+++ homework1/main.js
@@ -0,0 +1,77 @@
+let one, two, three, d, e, f, g, h, i, l, m, n, p;
+
+one = 'hello';
+console.log(one);
+alert(one);
+
+two = 'owu';
+console.log(two);
+alert(two);
+
+three = 'com';
+console.log(three);
+alert(three);
+
+d = 'ua';
+console.log(d);
+alert(d);
+
+e = 1;
+console.log(e);
+alert(e);
+
+f = 10;
+console.log(f);
+alert(f);
+
+g = -999;
+console.log(g);
+alert(g);
+
+h = 123;
+console.log(h);
+alert(h);
+
+i = 3.14;
+console.log(i);
+alert(i);
+
+l = 2.7;
+console.log(l);
+alert(l);
+
+m = 16;
+console.log(m);
+alert(m);
+
+n = true;
+console.log(n);
+alert(n);
+
+p = false;
+console.log(p);
+alert(p);
+
+document.write(one + ' ', two + ' ', three + ' ', d + ' ', e + ' ', f + ' ', g + ' ', h + ' ', i + ' ', l + ' ', m + ' ', n + ' ', p + ' ');
+
+
+let firstName = 'Mukhailo ';
+let middleName = 'Andriovych ';
+let lastName = 'Tryndiak';
+
+let person = `${firstName}${middleName}${lastName}`;
+console.log(person);
+console.log(firstName + middleName + lastName);
+
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
+
+let name = prompt('Write your name');
+let middlename = prompt('Write your middleame');
+console.log('My name is '+ name +' '+ middlename);
+let age = prompt('Write your age');
+console.log('I am ' + age);
